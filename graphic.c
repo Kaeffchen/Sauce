ca2fd97f93bc47caa4ba57a12bc0cc4c1aac7b5b
Jonas <legojonas@gmx.de>
2015-07-21 13:14:13 +0200

    Added display_game_area() nand clear_game_area() functions

--- graphic.c
+++ graphic.c
@@ -1,3 +1,21 @@
+/*graphic.c*/
+
+#include "graphic.h"
+
+int display_game_area( area[WIDTH][HEIGHT] )
+{
+	system("clear");
+	puts("\n\n");
+	for( int x = 0; x < WIDTH; x++ )
+	{
+		for( int y = 0; y < HEIGHT; y++ )
+		{
+			printf( "%c", area[x][y] );
+		}
+		putchar('\n');
+	}
+}
+
 int clear_game_area()
 {
     for( int x = 0; x < WIDTH; x++ )
--- graphic.h
+++ graphic.h
@@ -1,3 +1,10 @@
 /*graphic.h*/
 
-int clear_game_area(void);
+#define WIDTH 8
+#define HEIGHT 6
+#define NO_COIN 'O'
+#define COIN_ONE '|'
+#define COIN_TWO '-'
+
+int clear_game_area( int area[WIDTH][HEIGHT] );
+display_game_area( int area[WIDTH][HEIGHT] );
